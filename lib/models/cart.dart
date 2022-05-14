08fee70d6058996d6c394a336d22ee25387f75b1
Vivek Kadam <vivekadam1332@hotmail.com>
2022-05-14 15:59:46 +0530

    added a cart.dart

---
+++ lib/models/cart.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_for_trial/models/catalog.dart';
+
+class CartModel {
+  CatalogModel _cartlog;
+
+  final List<int> _itemIds = [];
+
+  CartModel get catalog => catalog;
+
+  set cataloh(CatalogModel newCatalog) {
+    _catalog = newCatalog;
+  }
+}
